6d6077b24c980e10b0bb2b82407d6d127d1060f8
Manemin <manemin80@gmail.com>
2020-08-24 12:21:18 +0300

    done

---
+++ lesson1/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+        <!-- <input type="text" v-model="text" @keyup.enter="add()" /> -->
+        <input type="text" v-model="text" />
+        <button @click="add()" v-show="todo.length < 10">add new</button>
+        <ul>
+            <li v-for="(el, i) in todo" :key="`${el}- ${i}`">
+                {{el}}
+                <button @click="del(i)">del</button>
+            </li>
+        </ul>
+        <div v-show="todo.length > 0">{{todo}}</div>
+    </div>
+</template>
+<script>
+export default {
+    name: "App",
+    data() {
+        return {
+            todo: [],
+            text: "",
+        };
+    },
+    methods: {
+        add() {
+            this.todo.push(this.text);
+            console.log(this.todo);
+        },
+        del(i) {
+            this.todo.splice(i, 1);
+            console.log(this.todo);
+        },
+    },
+};
+</script>
+
+<style>
+</style>
